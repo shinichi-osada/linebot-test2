83b53de5cb567b581b20ec06b7b420d4534e0565
shinichi-osada <osada.shi@gmail.com>
2016-11-26 06:12:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def worker():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        {
+        TextSendMessage{
     "type": "text",
     "text": "Hello, world"
 }
