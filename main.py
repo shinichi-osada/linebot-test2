d6b892d14553274f5ca34c38ee075aa489a7eba0
shinichi-osada <osada.shi@gmail.com>
2016-11-26 02:47:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ButtonsTemplate,
 )
 
 import config
