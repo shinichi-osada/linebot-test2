6fb594bc44b555bd360b5a34988e74714016ebdd
shinichi-osada <osada.shi@gmail.com>
2016-11-26 02:32:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def worker():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='tt-僕と契約して、魔法少女になってよ！' + body))
+        TextSendMessage(text='tt-僕と契約して、魔法少女になってよ！' ))
 
 
 if __name__ == "__main__":
