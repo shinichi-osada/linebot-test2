037abcb18ad32286242ad238787249d08df69c58
shinichi-osada <osada.shi@gmail.com>
2016-11-26 04:21:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -61,8 +61,28 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         ImageSendMessage(
-    original_content_url='http://pbs.twimg.com/profile_images/793221841653411840/gOettUfk_normal.jpg',
-    preview_image_url='http://pbs.twimg.com/profile_images/793221841653411840/gOettUfk_normal.jpg')
+    alt_text='Buttons template',
+    template=ButtonsTemplate(
+        thumbnail_image_url='https://example.com/image.jpg',
+        title='Menu',
+        text='Please select',
+        actions=[
+            PostbackTemplateAction(
+                label='postback',
+                text='postback text',
+                data='action=buy&itemid=1'
+            ),
+            MessageTemplateAction(
+                label='message',
+                text='message text'
+            ),
+            URITemplateAction(
+                label='uri',
+                uri='http://example.com/'
+            )
+        ]
+    )
+)
 )
 
 
