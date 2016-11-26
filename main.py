da3fbbdd213945a968bee66a6cb1518383dd777c
shinichi-osada <osada.shi@gmail.com>
2016-11-26 15:15:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -60,10 +60,7 @@ def worker():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        StickerSendMessage(
-    package_id='1',
-    sticker_id='1'
-)
+        StickerSendMessage( package_id='1', sticker_id='1' )
     )
 
 if __name__ == "__main__":
