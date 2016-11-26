77365490e1c0718947f0379d51d2b729da79384a
shinichi-osada <osada.shi@gmail.com>
2016-11-27 00:44:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def worker():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        ImageSendMessage(original_content_url="https://pbs.twimg.com/media/CxJGEZEUsAE_eIa.jpg"))
+        ImageSendMessage(original_content_url="https://pbs.twimg.com/media/CxJGEZEUsAE_eIa.jpg",preview_image_url="https://pbs.twimg.com/media/CxJGEZEUsAE_eIa.jpg"))
 
 if __name__ == "__main__":
     app.run()
