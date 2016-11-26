301063fd8e7b6cbae2f3d7675436721e26e0ee94
shinichi-osada <osada.shi@gmail.com>
2016-11-26 09:59:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -61,8 +61,6 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="sss")
-        TextSendMessage(text="aaa")
-
     )
 
 if __name__ == "__main__":
