0ad279de758252a4c56a4949fa242f4b673f212f
shinichi-osada <osada.shi@gmail.com>
2016-11-26 15:18:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,23 @@ def worker():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        StickerSendMessage( package_id='1', sticker_id='1' )
+        TemplateSendMessage(
+    alt_text='Confirm template',
+    template=ConfirmTemplate(
+        text='Are you sure?',
+        actions=[
+            PostbackTemplateAction(
+                label='postback',
+                text='postback text',
+                data='action=buy&itemid=1'
+            ),
+            MessageTemplateAction(
+                label='message',
+                text='message text'
+            )
+        ]
+    )
+)
     )
 
 if __name__ == "__main__":
