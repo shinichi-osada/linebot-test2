45a66c50bfedc1825a982eefb2d03123ce7eee29
shinichi-osada <osada.shi@gmail.com>
2016-11-27 23:44:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,6 @@
 # 書き込む文字列
 str = """ I'll be puzzled about what I should do from now on."""
  
-f = open('text.txt', 'w') # 書き込みモードで開く
-f.write(str) # 引数の文字列をファイルに書き込む
-f.close() # ファイルを閉じる
+f = open('text.txt', 'w') 
+f.write(str) 
+f.close() 
