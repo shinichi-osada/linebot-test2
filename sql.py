fc84d0277206abadec40d5f88573c898055647d1
shinichi-osada <osada.shi@gmail.com>
2016-12-01 16:11:12 +0900

    Create sql.py

---
+++ sql.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*- 
+
+import MySQLdb
+
+connector = MySQLdb.connect(host="localhost", db="sdb", user="root", passwd="32103210", charset="utf8")
+cursor = connector.cursor()
+
+sql = u"insert into test_table values('1','python')"
+cursor.execute(sql)
+sql = u"insert into test_table values('2','パイソン')"
+cursor.execute(sql)
+sql = u"insert into test_table values('3','ぱいそん')"
+cursor.execute(sql)
+
+connector.commit()
+
+cursor.close()
+connector.close()
